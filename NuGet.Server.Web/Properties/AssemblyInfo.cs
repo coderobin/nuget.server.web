7110fdf02b78e685450297cc3fe4527940ee52ed
Dong Xie <xied75@gmail.com>
2018-07-13 14:43:15 +0100

    1, up NuGet.Server from 2.11.2 -> 3.1.2 2, move applicationhost.config from solution items into project

--- NuGet.Server.Web/Properties/AssemblyInfo.cs
+++ NuGet.Server.Web/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NuGet.Server.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aa1c1198-2b9c-4ac2-86ae-7f8027cbcab8")]
+[assembly: Guid("27225a72-9de3-4ee2-aa2a-2b51baabb520")]
 
 // Version information for an assembly consists of the following four values:
 //
